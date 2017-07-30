db108ce9c9c94ed08a88fe8d5416a998584f0fdf
Mayank Kapoor <mayankkapoor@gmail.com>
2017-07-30 08:19:14 +0530

    Add basic test framework

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express')
 var app = express()
 
+exports.addTwoNumbers = function(number1, number2) {
+  return number1 + number2;
+}
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
